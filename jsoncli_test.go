11b43fdf97a95d5b61e3a9c872885392e0d6eb21
Ben Schwartz <benschw@gmail.com>
2015-01-22 21:03:48 -0600

    feat(*): cleaning and simplifying with typeswitches

--- jsoncli_test.go
+++ jsoncli_test.go
@@ -1,45 +1,10 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
 var b = []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"],"Address":{"Street":"0001 Cemetery Lane"}}`)
-
-func Test_parseJson(t *testing.T) {
-	// given
-
-	expected := map[string]interface{}{
-		"Name": "Wednesday",
-		"Age":  6,
-		"Parents": []interface{}{
-			"Gomez",
-			"Morticia",
-		},
-	}
-
-	// when
-	actual, err := parseBytes(b)
-
-	// then
-	if err != nil {
-		t.Errorf("Parse Error: %s", err)
-	}
-
-	m, _ := getMap(actual)
-
-	if expected["Name"] != m["Name"] {
-		t.Errorf("unexpected parsed obj:\n%+v\n%+v", expected, m)
-	}
-
-	m["Parents"], _ = getSlice(m["Parents"])
-
-	if !reflect.DeepEqual(expected["Parents"], m["Parents"]) {
-		t.Errorf("unexpected parsed obj:\n%+v\n%+v", expected, m)
-	}
-
-}
 
 func Test_valueSelector(t *testing.T) {
 	// given
@@ -48,8 +13,8 @@ func Test_valueSelector(t *testing.T) {
 	selector := "Name"
 
 	// when
-	found, err := selectValue(obj, selector)
-
+	i, _ := selectValue(obj, selector)
+	found, err := formatForDisplay(i)
 	// then
 
 	if err != nil {
@@ -57,7 +22,7 @@ func Test_valueSelector(t *testing.T) {
 	}
 
 	if found != expected {
-		t.Errorf("Found value wrong: %s", found)
+		t.Errorf("Found value wrong: %s:%s", expected, found)
 	}
 }
 func Test_sliceSelector(t *testing.T) {
@@ -67,7 +32,8 @@ func Test_sliceSelector(t *testing.T) {
 	selector := "Parents.1"
 
 	// when
-	found, err := selectValue(obj, selector)
+	i, _ := selectValue(obj, selector)
+	found, err := formatForDisplay(i)
 
 	// then
 
@@ -87,7 +53,29 @@ func Test_mapSelector(t *testing.T) {
 	selector := "Address.Street"
 
 	// when
-	found, err := selectValue(obj, selector)
+	i, _ := selectValue(obj, selector)
+	found, err := formatForDisplay(i)
+
+	// then
+
+	if err != nil {
+		t.Errorf("Parse Error: %s", err)
+	}
+
+	if found != expected {
+		t.Errorf("Found value wrong: %s", found)
+	}
+}
+
+func Test_jsonSelector(t *testing.T) {
+	// given
+	obj, _ := parseBytes(b)
+	expected := `["Gomez","Morticia"]`
+	selector := "Parents"
+
+	// when
+	i, _ := selectValue(obj, selector)
+	found, err := formatForDisplay(i)
 
 	// then
 
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"strconv"
@@ -18,33 +19,72 @@ func parseBytes(b []byte) (interface{}, error) {
 	return obj, err
 }
 
-func getMap(in interface{}) (map[string]interface{}, error) {
-	return in.(map[string]interface{}), nil
+func formatForJsonDisplay(i interface{}) (string, error) {
+	b, err := json.Marshal(i)
+	if err != nil {
+		return "", err
+	}
+	return string(b[:]), nil
+}
+func formatForDisplay(i interface{}) (string, error) {
+	switch v := i.(type) {
+	case int:
+		return strconv.Itoa(v), nil
+	case float64:
+		return strconv.FormatFloat(v, 'f', 6, 64), nil
+	case string:
+		return v, nil
+	case []interface{}:
+		return formatForJsonDisplay(i)
+	case map[string]interface{}:
+		return formatForJsonDisplay(i)
+	default:
+		return "", errors.New("unknown type")
+	}
 }
 
-func getSlice(in interface{}) ([]interface{}, error) {
-	return in.([]interface{}), nil
-}
-
-func selectValue(obj interface{}, selector string) (string, error) {
+func selectValue(obj interface{}, selector string) (interface{}, error) {
 	parts := strings.Split(selector, ".")
 
 	if len(parts) == 1 && parts[0] == "" {
-		return fmt.Sprintf("%s", obj), nil
-
-	} else if i, err := strconv.Atoi(parts[0]); err == nil {
-		s, err := getSlice(obj)
-		if err != nil {
-			return "", err
-		}
-		return selectValue(s[i], strings.Join(parts[1:], "."))
-	} else {
-		s, err := getMap(obj)
-		if err != nil {
-			return "", err
-		}
-		return selectValue(s[parts[0]], strings.Join(parts[1:], "."))
+		return obj, nil
 	}
+
+	switch v := obj.(type) {
+	case int:
+		return "", errors.New("selector not valid")
+	case float64:
+		return "", errors.New("selector not valid")
+	case string:
+		return "", errors.New("selector not valid")
+	case []interface{}:
+		i, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return "", err
+		}
+		return selectValue(v[i], strings.Join(parts[1:], "."))
+	case map[string]interface{}:
+		return selectValue(v[parts[0]], strings.Join(parts[1:], "."))
+	default:
+		return "", errors.New("unknown type")
+	}
+
+	// if len(parts) == 1 && parts[0] == "" {
+	// 	return fmt.Sprintf("%s", obj), nil
+
+	// } else if i, err := strconv.Atoi(parts[0]); err == nil {
+	// 	s, err := getSlice(obj)
+	// 	if err != nil {
+	// 		return "", err
+	// 	}
+	// 	return selectValue(s[i], strings.Join(parts[1:], "."))
+	// } else {
+	// 	s, err := getMap(obj)
+	// 	if err != nil {
+	// 		return "", err
+	// 	}
+	// 	return selectValue(s[parts[0]], strings.Join(parts[1:], "."))
+	// }
 
 }
 
