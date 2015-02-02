aba19ae3232e773d2a87da4b8ba8870532b681e8
Ben Schwartz <benschw@gmail.com>
2015-02-02 16:24:16 -0600

    chore(build): fixing tests

--- cli-unitw.sh
+++ cli-unitw.sh
@@ -5,7 +5,7 @@ BIN_PATH=".cli-unit/cli-unit"
 
 if [ ! -f $BIN_PATH ]; then
 	mkdir -p $(dirname $BIN_PATH)
-    curl -sS https://drone.io/github.com/benschw/cli-unit/files/cli-unit > $BIN_PATH
+    wget -qO- https://drone.io/github.com/benschw/cli-unit/files/cli-unit > $BIN_PATH
     chmod +x $BIN_PATH
 fi
 
